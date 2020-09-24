631aeb669d6f47add869c7ce5ebdc7ab06e8a1c5
savokin <locovname@gmail.com>
2020-09-24 09:09:19 -0700

    fd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+//get bill input
+const tipForm = document.getElementById("tip-form"); //global scope
+
+tipForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const billAmount = Number(document.querySelector("#bill-amount").value); //scoped to functionm, function scoped
+  const tipPercentage = Number(document.querySelector("#tip-amount").value);
+  //do some math for the tip
+  //do some math for tip plus bill
+  const tipAmount = document.querySelector("#final-tip");
+  tipAmount.innerHTML = 
+  //reset the calculator
+});
+
+//get tip percentage
+
+//process the values (actual calulations) get the tip
+
+//display to the user
